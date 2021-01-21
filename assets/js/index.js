ded1614ac444914b100fd6ff12943370bb24ec85
MoNiSu <monisu2001@gmail.com>
2021-01-21 15:54:41 +0900

    Add list

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const list = document.getElementById('list');
+
+function makeListVisible () {
+  list.classList.remove('hidden');
+}
+
+function makeListInvisible () {
+  list.classList.add('hidden');
+}
