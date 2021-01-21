a59041e2fd516d435d9d1568918fde6cf2ee8575
MoNiSu <monisu2001@gmail.com>
2021-01-21 21:10:00 +0900

    Add layer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,14 @@
 const list = document.querySelector('.list');
+const layer = document.querySelector('.layer');
 
 function makeListVisible () {
   list.style.transition = '0.3s';
   list.style.transform = "translate(-350px, 0px)";
+  layer.classList.remove('hidden');
 }
 
 function makeListInvisible () {
   list.style.transition = '0.3s';
   list.style.transform = "translate(350px, 0px)";
+  layer.classList.add('hidden');
 }
