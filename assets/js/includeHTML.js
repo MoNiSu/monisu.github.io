0b97f6980d071b0e7fafc8b59608c4cd1f3295d5
MoNiSu <monisu2001@gmail.com>
2021-01-23 17:31:35 +0900

    Add includeHTML module

---
+++ assets/js/includeHTML.js
@@ -0,0 +1,41 @@
+function includeHTML () {
+  let element = document.getElementsByTagName('import');
+
+  for (let i = 0; i < element.length; i++) {
+    let href = element[i].getAttribute('href');
+    let parent = element[i].parentNode;
+    let tagLocation;
+
+    for (let j = 0; j < parent.childNodes.length; j++) {
+      if (parent.childNodes[j] == element[i]) {
+        tagLocation = parent.childNodes[j];
+        break;
+      }
+    }
+
+    if (href) {
+      let xhr = new XMLHttpRequest;
+
+      xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          if (this.status === 200) {
+            element[i].innerHTML = this.responseText;
+            parent.insertBefore(element[i].firstChild, tagLocation);
+          } else if (this.status === 404) {
+            console.log(`${href} is Not Found.`);
+          }
+
+          element[i].remove();
+          importHTML();
+        }
+      }
+
+      xhr.open('GET', href, true);
+      xhr.send();
+
+      return;
+    }
+  }
+}
+
+includeHTML();
