8c0bcc20170852d8496c2eb94654e98a501edc62
MoNiSu <monisu2001@gmail.com>
2021-01-23 18:37:42 +0900

    Combine head and change tag name

--- assets/js/includeHTML.js
+++ assets/js/includeHTML.js
@@ -1,17 +1,8 @@
 function includeHTML () {
-  let element = document.getElementsByTagName('import');
+  let element = document.getElementsByTagName('include');
 
   for (let i = 0; i < element.length; i++) {
     let href = element[i].getAttribute('href');
-    let parent = element[i].parentNode;
-    let tagLocation;
-
-    for (let j = 0; j < parent.childNodes.length; j++) {
-      if (parent.childNodes[j] == element[i]) {
-        tagLocation = parent.childNodes[j];
-        break;
-      }
-    }
 
     if (href) {
       let xhr = new XMLHttpRequest;
@@ -19,8 +10,7 @@ function includeHTML () {
       xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
           if (this.status === 200) {
-            element[i].innerHTML = this.responseText;
-            parent.insertBefore(element[i].firstChild, tagLocation);
+            element[i].insertAdjacentHTML('afterend', this.responseText);
           } else if (this.status === 404) {
             console.log(`${href} is Not Found.`);
           }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,19 @@
-const menu = document.querySelector('.menu');
-const menuLayer = document.querySelector('.menu_layer');
+// const menu = document.querySelector('.menu');
+// const menuLayer = document.querySelector('.menu_layer');
 
 function openMenu () {
+  let menu = document.querySelector('.menu');
+  let menuLayer = document.querySelector('.menu_layer');
+
   menu.style.transition = '0.3s';
   menu.style.transform = "translate(-350px, 0px)";
   menuLayer.classList.remove('hidden');
 }
 
 function closeMenu () {
+  let menu = document.querySelector('.menu');
+  let menuLayer = document.querySelector('.menu_layer');
+  
   menu.style.transition = '0.3s';
   menu.style.transform = "translate(350px, 0px)";
   menuLayer.classList.add('hidden');
