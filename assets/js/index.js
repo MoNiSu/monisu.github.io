35e1480a6db538b88ed51188767f3920bc87567e
MoNiSu <monisu2001@gmail.com>
2021-01-27 02:22:11 +0900

    Change nav design and some designs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,25 @@
-// const menu = document.querySelector('.menu');
-// const menuLayer = document.querySelector('.menu_layer');
+// const nav = document.querySelector('nav');
+// const navLayer = document.querySelector('.nav_layer');
 
-function openMenu () {
-  let menu = document.querySelector('.menu');
-  let menuLayer = document.querySelector('.menu_layer');
-
-  menu.style.transition = '0.3s';
-  menu.style.transform = "translate(-350px, 0px)";
-  menuLayer.classList.remove('hidden');
+function openNav (nav, navLayer) {
+  nav.style.transition = '0.3s';
+  nav.style.transform = `translate( ${nav.offsetWidth * -1}px, 0px )`;
+  navLayer.classList.remove('hidden');
 }
 
-function closeMenu () {
-  let menu = document.querySelector('.menu');
-  let menuLayer = document.querySelector('.menu_layer');
-  
-  menu.style.transition = '0.3s';
-  menu.style.transform = "translate(350px, 0px)";
-  menuLayer.classList.add('hidden');
+function closeNav (nav, navLayer) {
+  nav.style.transition = '0.3s';
+  nav.style.transform = `translate( ${nav.offsetWidth}px, 0px )`;
+  navLayer.classList.add('hidden');
+}
+
+function changeNavStatus () {
+  let nav = document.querySelector('nav');
+  let navLayer = document.querySelector('.nav_layer');
+
+  if (navLayer.classList.contains('hidden')) {
+    openNav(nav, navLayer);
+  } else {
+    closeNav(nav, navLayer);
+  }
 }
